dc52249d9ce6bf8ab80c6fd0c744c4d0a8891e6d
Atomimox <atomimox05stalker@gmail.com>
2025-03-12 23:36:40 -0400

    Item agregado con éxito, solucionar instancia de bloque

--- src/main/java/com/ax5/mccsj/MetroDeCaracas.java
+++ src/main/java/com/ax5/mccsj/MetroDeCaracas.java
@@ -1,6 +1,8 @@
 package com.ax5.mccsj;
 
 import com.ax5.mccsj.block.ModBlocks;
+import com.ax5.mccsj.block.ModBlocks;
+import com.ax5.mccsj.items.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,7 +14,8 @@ public class MetroDeCaracas implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		ModBlocks.registerModBlocks();
+		ModBlocks.initialize();
+		ModItems.init();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/com/ax5/mccsj/items/ModItems.java
+++ src/main/java/com/ax5/mccsj/items/ModItems.java
@@ -1,25 +1,24 @@
-/*package com.ax5.mccsj.block;
+package com.ax5.mccsj.items;
 
 import com.ax5.mccsj.MetroDeCaracas;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 import java.util.function.Function;
 
-public class ModBlocks {
-    public static void initialize() {
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register((itemGroup) -> {
-            itemGroup.add(ModBlocks.CONDENSED_DIRT.asItem());
-        });
+public class ModItems {
+
+    public static void init(){
+        // Get the event for modifying entries in the ingredients group.
+        // And register an event handler that adds our suspicious item to the ingredients group.
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE));
     }
 
     public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
@@ -35,15 +34,7 @@ public class ModBlocks {
         return item;
     }
 
-    public static Block registerBlock(String name, Block block) {
-        Registry.register(Registries.BLOCK, Identifier.of(MetroDeCaracas.MOD_ID, name), block);
-        return block;
-    }
+    public static final Item SUSPICIOUS_SUBSTANCE = register("suspicious_substance", Item::new, new Item.Settings());
 
-    public static final Block CONDENSED_DIRT = register(
-            "condensed_dirt",
-            Block::new,
-            AbstractBlock.Settings.create().sounds(BlockSoundGroup.GRASS),
-            true
-    );
-}*/
+
+}
