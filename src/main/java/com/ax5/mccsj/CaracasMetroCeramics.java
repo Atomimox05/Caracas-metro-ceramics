f37a0db9f79d0b0b6698ad4239a3531ead216f91
atomimox <atomimox05stalker@gmail.com>
2025-04-11 14:56:14 -0400

    Creando item

--- src/main/java/com/ax5/mccsj/CaracasMetroCeramics.java
+++ src/main/java/com/ax5/mccsj/CaracasMetroCeramics.java
@@ -1,5 +1,6 @@
 package com.ax5.mccsj;
 
+import com.ax5.mccsj.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class CaracasMetroCeramics implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello world!");
+		ModItems.registerModItems();
 	}
 }
